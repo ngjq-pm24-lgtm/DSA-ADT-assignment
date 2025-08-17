a6995ac60f6f1ca3369dcf5d5226f836a983afcb
jef123gaming <119925111+jef123gaming@users.noreply.github.com>
2025-08-17 22:19:57 +0800

    Add files via upload

---
+++ ClinicManagementSystem/src/main/java/ClinicManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+
+/**
+ *
+ * @author Admin
+ */
+public class ClinicManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
